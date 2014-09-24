2aff9bb8db4e3aad1e31e9182a1122920bd7eaf9
pdrockz <pedro.nardo@fatec.sp.gov.br>
2014-09-24 16:03:30 -0300

    Deletando o projeto "CadastroCaelum"

--- CadastroCaelum/src/br/com/caelum/cadastro/Extras.java
+++
@@ -1,8 +0,0 @@
-package br.com.caelum.cadastro;
-
-public class Extras {
-	//Classe que tem como finalidade evitar erros de digitacao quando e colocado e buscado extras 
-	//nas intents, 
-	public static final String ALUNO_SELECIONADO = "alunoselecionado";
-	
-}
